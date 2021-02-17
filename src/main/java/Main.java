d29728f14d1c08b7fc03e3f93183f5f13b530232
Devx <fabio.xavier@discente.ufg.br>
2021-02-17 10:26:53 -0300

    Handle input data and context of application

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,47 @@
+import br.com.devx.trade.business.Classifier;
+import br.com.devx.trade.business.DefaultedClassifier;
+import br.com.devx.trade.business.HighRiskClassifier;
+import br.com.devx.trade.business.MediumRiskClassifier;
+import br.com.devx.trade.domain.Context;
+import br.com.devx.trade.domain.ITrade;
+import br.com.devx.trade.util.DateUtil;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+
+    private static Scanner INPUT = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Works!");
+
+        String refDateStr = INPUT.next();
+        System.out.println(refDateStr);
+        LocalDate refDate = DateUtil.toLocalDate(refDateStr);
+        Integer countTraders = INPUT.nextInt();
+
+        List<ITrade> trades = loadTrades(countTraders);
+        Context context = new Context(refDate, trades);
+        context.checkRiskAnalysis();
+    }
+
+    private static List<ITrade> loadTrades(Integer countTraders) {
+        List<ITrade> trades = new ArrayList<>();
+        for (int i = 0; i < countTraders; i++) {
+            ITrade trade = loadTrade();
+            trades.add(trade);
+        }
+        return trades;
+    }
+
+    private static ITrade loadTrade() {
+        double value = INPUT.nextDouble();
+        String sector = INPUT.next();
+        String nextPaymentStr = INPUT.next();
+        LocalDate nextPayment = DateUtil.toLocalDate(nextPaymentStr);
+        ITrade trade = new ITrade(value, sector, nextPayment);
+        return trade;
     }
 }
